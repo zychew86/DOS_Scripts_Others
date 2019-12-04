44f6d579a727e8b534b9ede3c32fb9c30e0f96e5
zychew86 <58324899+zychew86@users.noreply.github.com>
2019-12-04 09:22:56 +0800

    update ip

--- baseline_http.sh
+++ baseline_http.sh
@@ -2,22 +2,22 @@
 SECONDS=0
 while true; do
    for i in "" index.html; do
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
-      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 203.134.121.74/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
+      curl -m 1 -s -o /dev/null -w "/$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" 10.1.20.20/$i
    done
-   hping3 --quiet -1 -c 5 --fast 203.134.121.74
-   hping3 --quiet -1 -c 5 --fast 203.131.121.74
+   hping3 --quiet -1 -c 5 --fast 10.1.20.20
+   hping3 --quiet -1 -c 5 --fast 10.1.20.20
 sleep 1
 clear
 echo "Baselining. Use 'tmsh show security dos l4bdos-context-info' to see status."
